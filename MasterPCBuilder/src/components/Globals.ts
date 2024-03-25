ba76fea7a104d9643c98474ad89137c0135d661a
JulioGlezGlez <juliomanuelgonzalezgonzalez@gmail.com>
2024-03-25 15:45:03 +0100

    fix(main): When tried to merge there were some problem that are now solved

--- MasterPCBuilder/src/components/Globals.ts
+++ MasterPCBuilder/src/components/Globals.ts
@@ -4,8 +4,9 @@ export class Globals {
     static IP_CASA_JULIO: string = "192.168.1.100:8080";
     static IP_PORTATIL_JULIO: string = "192.168.0.138:8080";
     static IP_PORTATIL_TRABAJO: string = "192.168.1.61:8080";
-    static IP_HTTP: string = "http://" + this.IP_PORTATIL_TRABAJO;
-    static IP_WEBSOCKET: string = "ws://" + this.IP_PORTATIL_TRABAJO;
+    static IP_DIGITAL_OCEAN: string = "146.190.12.221:8080";
+    static IP_HTTP: string = "http://" + this.IP_DIGITAL_OCEAN;
+    static IP_WEBSOCKET: string = "ws://" + this.IP_DIGITAL_OCEAN;
     static NOT_ACTIVE: string = "not active";
     static INC_PASS_USR: string = "User/Pass incorrect";
     static IMG_NOT_FOUND: string = "The file does not exist";
