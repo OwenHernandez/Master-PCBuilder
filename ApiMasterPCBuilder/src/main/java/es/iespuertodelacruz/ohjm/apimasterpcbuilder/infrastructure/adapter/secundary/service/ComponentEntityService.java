2abe895da1dfb897156351dc196443aff14fb419
JulioGlezGlez <juliomanuelgonzalezgonzalez@gmail.com>
2024-04-04 12:40:31 +0200

    fix(develop): The ip to connect to the scrapper

--- ApiMasterPCBuilder/src/main/java/es/iespuertodelacruz/ohjm/apimasterpcbuilder/infrastructure/adapter/secundary/service/ComponentEntityService.java
+++ ApiMasterPCBuilder/src/main/java/es/iespuertodelacruz/ohjm/apimasterpcbuilder/infrastructure/adapter/secundary/service/ComponentEntityService.java
@@ -36,7 +36,7 @@ public class ComponentEntityService implements IComponentRepository {
     Logger log;
     @Autowired
     public ComponentEntityService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("http://146.190.12.221:8000").build();
+        this.webClient = webClientBuilder.baseUrl("http://localhost:8000").build();
     }
     @Value("${fastapi.apikey}")
     private String apiKey;
