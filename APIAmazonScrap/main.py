207a7d62cebe833afb9530cbf2a932802018f6f8
JulioGlezGlez <juliomanuelgonzalezgonzalez@gmail.com>
2024-04-04 10:23:15 +0200

    feat(develop): Put an info for the products being search

--- APIAmazonScrap/main.py
+++ APIAmazonScrap/main.py
@@ -5,25 +5,29 @@ from selectorlib import Extractor
 import requests
 import json
 from time import sleep
+
 app = FastAPI()
 
 e = Extractor.from_yaml_file('search_results.yml')
 
+
 @app.get("/{search}")
-async def root(search:str):
+async def root(search: str):
     # product_data = []
-    amazon_url = "https://www.amazon.com/s?k="+search
+    amazon_url = "https://www.amazon.com/s?k=" + search
     proxies_list = open("rotating_ip.txt", "r").read().strip().split("\n")
-    numero=random.randint(0, len(proxies_list)-1)
-    data = scrape(amazon_url,proxies_list[numero])
-    products= []
+    numero = random.randint(0, len(proxies_list) - 1)
+    data = scrape(amazon_url, proxies_list[numero])
+    products = []
     if data:
         for product in data['products']:
+            print("Product: %s" % product['title'])
             product['search_url'] = amazon_url
             products.append(product)
         return products
 
-def scrape(url,proxy):
+
+def scrape(url, proxy):
     headers = {
         'dnt': '1',
         'upgrade-insecure-requests': '1',
@@ -41,15 +45,13 @@ def scrape(url,proxy):
 
     # Download the page using requests
     print("Downloading %s" % url)
-    r = requests.get(url,proxies={'http': f"http://{proxy}"}, headers=headers)
+    r = requests.get(url, proxies={'http': f"http://{proxy}"}, headers=headers)
     # Simple check to check if page was blocked (Usually 503)
     if r.status_code > 500:
         if "To discuss automated access to Amazon data please contact" in r.text:
-            print(r.text)
             print("Page %s was blocked by Amazon. Please try using better proxies\n" % url)
         else:
             print("Page %s must have been blocked by Amazon as the status code was %d" % (url, r.status_code))
         return None
     # Pass the HTML of the page and create
     return e.extract(r.text)
-
