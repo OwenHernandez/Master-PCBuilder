52bd0fd346bcdd9d15f40c1ac237ae53f5322fd0
JulioGlezGlez <juliomanuelgonzalezgonzalez@gmail.com>
2024-04-11 22:54:51 +0200

    feat(feaure_admin_web): add the login with some style

---
+++ MasterPCBuilderAdmin/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Router from './Components/Router';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ MasterPCBuilderAdmin/src/Components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Home = (props: Props) => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default Home
