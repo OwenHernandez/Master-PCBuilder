e37f3982f0eb9d2306c5e0c92eb1a55fff78489f
JulioGlezGlez <juliomanuelgonzalezgonzalez@gmail.com>
2024-04-16 17:05:35 +0200

    feat(feature_admin_web): Tried to put doughnuts together for the home

--- MasterPCBuilderAdmin/src/Components/Home.tsx
+++ MasterPCBuilderAdmin/src/Components/Home.tsx
@@ -1,11 +1,46 @@
 import React from 'react'
+import { Doughnut } from 'react-chartjs-2';
+import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';
+
+Chart.register(ArcElement, Tooltip, Legend);
 
 type Props = {}
-
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 const Home = (props: Props) => {
   return (
-    <div className="text-3xl font-bold underline bg-white h-full">
-      <h1>Hello world!</h1>
+    <div className="flex-1 text-3xl font-bold underline bg-white h-full">
+      <div className='flex-1 flex-col'>
+        <div className='flex-1 '>
+          <Doughnut data={data} />
+        </div>
+        <div className='flex-1'>
+          <Doughnut data={data} />
+        </div>
+      </div>
     </div>
   )
 }
--- MasterPCBuilderAdmin/src/Components/Login.tsx
+++ MasterPCBuilderAdmin/src/Components/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import logo from '../img/logo_transparent.png'
 import axios from 'axios'
@@ -8,6 +8,7 @@ type Props = {}
 
 const Login = (props: Props) => {
     const { setToken, setIsLoged } = useAppContext();
+    const [isWorking, setIsWorking] = useState<number>(0);
     const navigate = useNavigate();
     async function login(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault()
@@ -17,7 +18,6 @@ const Login = (props: Props) => {
 
         try {
             console.log(Globals.IP_HTTP + '/api/v1/login');
-
             const response = await axios.post('http://localhost:8080/api/v1/login', {
                 nick: nickname,
                 password: password
@@ -27,48 +27,51 @@ const Login = (props: Props) => {
                 const responseUser = await axios.get('http://localhost:8080/api/v2/users?nick=' + nickname, { headers: { 'Authorization': "Bearer " + data } });
                 const dataUser = responseUser.data;
                 if (dataUser.role === "ROLE_ADMIN" && dataUser.active === true) {
+                    setIsWorking(1);
                     setToken(data);
                     setIsLoged(true);
                     navigate('/home');
                 } else {
-                    alert("You are not an admin");
+
                 }
             }
         } catch (error) {
-            console.error(error)
+            window.alert("Se ha producido un error al intentar logearse.");
         }
 
     }
     return (
-        <section className="bg-gray-50 dark:bg-gradient-to-b from-gray-900 via-gray-600 to-gray-400  h-100">
-            <div className="flex flex-col items-center justify-center px-6 py-8  md:h-screen ">
-                <a href="#" className="flex items-center mb-2 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <img src={logo} alt="logo" className="w-20 h-20" />
-                    MasterPCBuilder
-                </a>
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-700 dark:border-gray-900">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Login
-                        </h1>
-                        <form className="space-y-4 md:space-y-6" onSubmit={login}>
-                            <div>
-                                <label htmlFor="nickname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your nickname</label>
-                                <input type="nickname" name="nickname" id="nickname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nickname" required={true} />
-                            </div>
-                            <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true} />
-                            </div>
-                            <div>
-                                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
-                            </div>
-                        </form>
+        <div>
+
+            <section className="bg-gray-50 dark:bg-gradient-to-b from-gray-900 via-gray-600 to-gray-400  h-100">
+                <div className="flex flex-col items-center justify-center px-6 py-8  md:h-screen ">
+                    <a href="#" className="flex items-center mb-2 text-2xl font-semibold text-gray-900 dark:text-white">
+                        <img src={logo} alt="logo" className="w-20 h-20" />
+                        MasterPCBuilder
+                    </a>
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-700 dark:border-gray-900">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                Login
+                            </h1>
+                            <form className="space-y-4 md:space-y-6" onSubmit={login}>
+                                <div>
+                                    <label htmlFor="nickname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your nickname</label>
+                                    <input type="nickname" name="nickname" id="nickname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nickname" required={true} />
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true} />
+                                </div>
+                                <div>
+                                    <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     )
 }
-
-export default Login
+export default Login;
